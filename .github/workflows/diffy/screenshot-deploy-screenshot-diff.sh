cd89598eb35441f5c1c22d94e06a22a6bd78b775
Uladzislau Trehubau <76438574+u-trehubau@users.noreply.github.com>
2021-09-24 09:27:50 +0200

    Update screenshot-deploy-screenshot-diff.sh

--- .github/workflows/diffy/screenshot-deploy-screenshot-diff.sh
+++ .github/workflows/diffy/screenshot-deploy-screenshot-diff.sh
@@ -8,6 +8,10 @@ cp diffy.phar /usr/local/bin/diffy
 # Authorisation.
 diffy auth:login $DIFFY_API_KEY
 
+# Get API token.
+DIFFY_API_TOKEN=$(curl -X POST "$MAIN_API_URL/auth/key" -H "accept: application/json" -d '{"key":"'$DIFFY_API_KEY'"}')
+DIFFY_API_TOKEN=$(echo $DIFFY_API_TOKEN | jq -r '.token')
+
 #----------- NOT NEEDED -----------
 # First set of screenshots.
 #echo "Starting taking pre-deployment screenshots..."
@@ -28,8 +32,13 @@ diffy auth:login $DIFFY_API_KEY
 #----------- /NOT NEEDED -----------
 
 # Compare two environments.
-DIFF_ID=`diffy project:compare $DIFFY_PROJECT_ID $DIFFY_ENV1 $DIFFY_ENV2`
-echo "Diff started $DIFF_ID"
+DIFFY_DIFF_ID=`diffy project:compare $DIFFY_PROJECT_ID $DIFFY_ENV1 $DIFFY_ENV2`
+echo "Diff started $DIFFY_DIFF_ID"
+
+# Get link to diff.
+DIFFY_DIFF_LINK=$(curl -X GET "https://app.diffy.website/api/diffs/$DIFFY_DIFF_ID" -H "accept: application/json" -H "Authorization: Bearer $DIFFY_API_TOKEN")
+DIFFY_DIFF_LINK=$(echo $DIFFY_DIFF_LINK | jq -r '.diffSharedUrl')
 
 # Set ENV variables.
-echo "DIFF_ID=$DIFF_ID" >> $GITHUB_ENV
+echo "DIFFY_DIFF_ID=$DIFFY_DIFF_ID" >> $GITHUB_ENV
+echo "DIFFY_DIFF_LINK=$DIFFY_DIFF_LINK" >> $GITHUB_ENV
